8777a19907bc3edef4fe57e8b7a63ddaf55ba667
GWFreak01 <gwfreak01@gmail.com>
2018-06-20 17:39:23 -0400

    createUser functionality implemented with AuthService

--- src/app/auth/create-user/create-user.component.ts
+++ src/app/auth/create-user/create-user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
+import {AuthService} from '../auth.service';
 
 @Component({
   selector: 'app-create-user',
@@ -8,13 +9,21 @@ import {NgForm} from '@angular/forms';
 })
 export class CreateUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
 // TODO: Verify second password
   onCreateUser(createUserForm: NgForm) {
     console.log(createUserForm.value);
+    if (createUserForm.invalid) {
+      return;
+    } else {
+      this.authService.createUser(
+        createUserForm.value.username,
+        createUserForm.value.email,
+        createUserForm.value.password);
+    }
   }
 
 }
