f1664a37d29036d44736c33fba15bd857c6d52aa
GWFreak01 <gwfreak01@gmail.com>
2018-06-21 16:25:03 -0400

    Added dynamic company display for new event form

--- src/app/new-event/new-event.component.ts
+++ src/app/new-event/new-event.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Company} from '../../../backend/models/company.model';
+import {CompaniesService} from '../services/companies/companies.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-new-event',
   templateUrl: './new-event.component.html',
   styleUrls: ['./new-event.component.css']
 })
-export class NewEventComponent implements OnInit {
+export class NewEventComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  eventTypes = ['Quality', 'Delivery'];
+  companies: Company[] = [];
+  private companiesSub: Subscription;
+  constructor(private  companyService: CompaniesService) { }
 
   ngOnInit() {
+    this.companyService.getCompanies();
+    this.companiesSub = this.companyService.getCompanyUpdateListener()
+      .subscribe((companies: Company[]) => {
+        this.companies = companies;
+      });
+    // getCompaniesList()
+  }
+
+
+  ngOnDestroy() {
+    this.companiesSub.unsubscribe();
   }
 
 }
