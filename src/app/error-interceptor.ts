d83ce6140fc251a00ca9b597bf734110b85a719a
GWFreak01 <gwfreak01@gmail.com>
2018-06-25 22:38:28 -0400

    Removed Error Dialog. Now displays errors if thrown

--- src/app/error-interceptor.ts
+++ src/app/error-interceptor.ts
@@ -21,15 +21,15 @@ export class ErrorInterceptor implements HttpInterceptor {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
         let errorMessage = 'An unknown error occurred!';
-        if (error.error.error.message) {
-          errorMessage = error.error.error.message;
+        if (error.error.message) {
+          errorMessage = error.error.message;
           console.log(errorMessage);
         }
-        this.dialog.open(ErrorComponent, {
-          data: {
-            message: errorMessage
-          }
-        });
+        // this.dialog.open(ErrorComponent, {
+        //   data: {
+        //     message: errorMessage
+        //   }
+        // });
         return throwError(error);
       })
     );
