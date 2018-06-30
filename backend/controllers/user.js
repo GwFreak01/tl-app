bf7c52b418b9ef8bf94934870335f6c151a8c689
GWFreak01 <gwfreak01@gmail.com>
2018-06-30 15:03:47 -0400

    Adds initial admin account if does not exist on login

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -26,46 +26,120 @@ exports.createUser = (req, res, next) => {
 
 exports.loginUser = (req, res, next) => {
   let fetchedUser;
-  User.findOne({$or:[{username: req.body.username}, {email: req.body.email}]})
-    .then(user => {
+  if (req.body.username === 'tl_admin') {
+    User.findOne({$or: [{username: req.body.username}, {email: req.body.email}]}, function (error, user) {
       if (!user) {
-        return res.status(404).json({
-          message: 'No user found'
+        console.log(user);
+        bcrypt.hash(req.body.password, 10).then(hash => {
+          const user = new User({
+            username: req.body.username,
+            email: 'tl_admin@tl.com',
+            password: hash
+          });
+          user.save().then(result => {
+            console.log(result);
+            // res.status(201).json({
+            //   message: 'User Created',
+            //   result: result
+            // });
+          }).catch(err => {
+            console.log(err.message);
+            res.status(500).json({
+              message: err.message
+            });
+          });
+
         });
+      } else {
+        User.findOne({$or: [{username: req.body.username}, {email: req.body.email}]})
+          .then(user => {
+            if (!user) {
+              return res.status(404).json({
+                message: 'No user found'
+              });
+            }
+            fetchedUser = user;
+            return bcrypt.compare(req.body.password, user.password);
+          })
+          .then(result => {
+            if (!result) {
+              return res.status(404).json({
+                message: 'Password incorrect!'
+              });
+            }
+            const token = jwt.sign(
+              {
+                username: fetchedUser.username,
+                email: fetchedUser.email,
+                userId: fetchedUser._id
+              },
+              process.env.JWT_KEY,
+              {
+                expiresIn: '1h'
+              }
+            );
+            // console.log('Server.login.token: ', token);
+            return res.status(200).json({
+              token: token,
+              expiresIn: 3600
+            })
+          }, error => {
+            return res.status(404).json({
+              message: error.message
+            });
+          })
+          .catch(err => {
+            return res.status(404).json({
+              message: err.message
+            });
+          });
       }
-      fetchedUser = user;
-      return bcrypt.compare(req.body.password, user.password);
-    })
-    .then(result => {
-      if (!result) {
-        return res.status(404).json({
-          message: 'Password incorrect!'
-        });
-      }
-      const token = jwt.sign(
-        {
-          username: fetchedUser.username,
-          email: fetchedUser.email,
-          userId: fetchedUser._id
-        },
-        process.env.JWT_KEY,
-        {
-          expiresIn: '1h'
+
+    });
+  } else {
+    User.findOne({$or: [{username: req.body.username}, {email: req.body.email}]})
+      .then(user => {
+        if (!user) {
+          return res.status(404).json({
+            message: 'No user found'
+          });
         }
-      );
-      // console.log('Server.login.token: ', token);
-      return res.status(200).json({
-        token: token,
-        expiresIn: 3600
+        fetchedUser = user;
+        return bcrypt.compare(req.body.password, user.password);
       })
-    }, error => {
-      return res.status(404).json({
-        message: error.message
+      .then(result => {
+        if (!result) {
+          return res.status(404).json({
+            message: 'Password incorrect!'
+          });
+        }
+        const token = jwt.sign(
+          {
+            username: fetchedUser.username,
+            email: fetchedUser.email,
+            userId: fetchedUser._id
+          },
+          process.env.JWT_KEY,
+          {
+            expiresIn: '1h'
+          }
+        );
+        // console.log('Server.login.token: ', token);
+        return res.status(200).json({
+          token: token,
+          expiresIn: 3600
+        })
+      }, error => {
+        return res.status(404).json({
+          message: error.message
+        });
+      })
+      .catch(err => {
+        return res.status(404).json({
+          message: err.message
+        });
       });
-    })
-    .catch(err => {
-      return res.status(404).json({
-        message: err.message
-      });
-    })
+  }
 };
+
+
