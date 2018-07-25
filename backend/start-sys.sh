98e8f73ada66f3a832d8d1f6e36a392286b4e051
GWFreak01 <gwfreak01@gmail.com>
2018-07-25 16:19:57 -0400

    Adds deployment scripts for starting system containing backend and front end scripts.

---
+++ backend/start-sys.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+
+# Install NodeJS 10+
+# Install MongoDB 3.6.4+
+
+# sudo apt-get install -y mate-terminal
+# sudo npm install -g -y @angular/cli
+#
+
+mate-terminal --geometry=80x24+20+20 --zoom=1 --hide-menubar - e /home/tl/Documents/tl-app/Backend.sh &
+mate-terminal --geometry=80x24+20+400 --zoom=1 --hide-menubar - e /home/tl/Documents/tl-app/Frontend.sh &
+
+
+
