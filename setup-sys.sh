726e271c6b84ee13a6e48ad404b5a503914936bf
GWFreak01 <gwfreak01@gmail.com>
2018-07-27 13:02:27 -0400

    Test internal local deployment. Fixes typo and moves files to tmp folder
    * Fix QuarterlyCronJob to email all companies

--- setup-sys.sh
+++ setup-sys.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-cd /home/tl/Downloads/
-
+cd /tmp/
+rm master.zip
 wget https://github.com/GwFreak01/tl-app/archive/master.zip
-tar -xvf master.zip /tmp
+tar -xvf master.zip
 rm master.zip
 
-mv -f /tmp/tl-app/master/* /home/tl/Documents/tl-app
+mv -f /tmp/tl-app-master/* /home/tl/Documents/tl-app
 
 ng build --prod --build-optimizer
