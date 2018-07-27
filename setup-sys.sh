bbd580fa234c4db0d4b633ea95da9115998650a7
GWFreak01 <gwfreak01@gmail.com>
2018-07-27 13:42:17 -0400

    Test internal local deployment. Changes directories to tl-app directory to build properly. * Fix QuarterlyCronJob to email all companies

--- setup-sys.sh
+++ setup-sys.sh
@@ -3,14 +3,17 @@
 cd /tmp/
 rm master.zip
 wget https://github.com/GwFreak01/tl-app/archive/master.zip
-unzip master.zip
+unzip master.zip -y
 rm master.zip
 
 mv -f tl-app-master/* /home/tl/Documents/tl-app
 rm -rf tl-app-master
+
+cd /home/tl/Documents/tl-app/
+
 ng build --prod --build-optimizer
 
 read -p "Press [Enter] key to end..."
 
-$SHELL
+exec sh
 
