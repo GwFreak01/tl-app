e112195fccfd2fa6e985fef56d21ea32ee4ce94f
GWFreak01 <gwfreak01@gmail.com>
2018-07-27 13:14:13 -0400

    Test internal local deployment. Fixes setup-sys script from exiting after finished until key press.
    * Fix QuarterlyCronJob to email all companies

--- setup-sys.sh
+++ setup-sys.sh
@@ -3,9 +3,14 @@
 cd /tmp/
 rm master.zip
 wget https://github.com/GwFreak01/tl-app/archive/master.zip
-tar -xvf master.zip
+unzip master.zip
 rm master.zip
 
 mv -f /tmp/tl-app-master/* /home/tl/Documents/tl-app
 
 ng build --prod --build-optimizer
+
+read -p "Press [Enter] key to end..."
+
+$SHELL
+
