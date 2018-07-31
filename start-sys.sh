e5d8b45c89157ad7ba0f4e39e07f4ade474d09b0
GWFreak01 <gwfreak01@gmail.com>
2018-07-31 15:44:05 -0400

    Adds killall node processes before re-serving.
    * Fix QuarterlyCronJob to email all companies
    * Fix sendAllFeedback button function

--- start-sys.sh
+++ start-sys.sh
@@ -8,7 +8,7 @@
 # sudo npm install -g -y @angular/cli
 
 #
-
+killall node
 mate-terminal --geometry=100x24+20+20 --zoom=1 --hide-menubar -e 'sh /home/tl/Documents/tl-app/Backend.sh' &
 mate-terminal --geometry=100x24+20+400 --zoom=1 --hide-menubar -e 'sh /home/tl/Documents/tl-app/Frontend.sh' &
 
