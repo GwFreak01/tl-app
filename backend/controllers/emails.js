920d6847307cdb3cd5a7c5fa132ab3f618f32299
GWFreak01 <gwfreak01@gmail.com>
2018-08-01 13:51:15 -0400

    Tests sendAllFeedback API call. Adds 5 second delay.
    * Fix QuarterlyCronJob to email all companies
    * Fix sendAllFeedback button function

--- backend/controllers/emails.js
+++ backend/controllers/emails.js
@@ -429,110 +429,112 @@ exports.sendAllFeedbackEmails = (req, res, next) => {
     emailList.push(companyEmails);
     eventsList.push(companyEvents);
 
-    setTimeout(() => {}, 5000);
-
-    readHTMLFile(path.join(__dirname, '../models/html_templates/companyReport.html'), function (err, html) {
-
-
-      const emailReplacements = companyEvents;
-
-      handlebars.registerHelper('ifEventBad', function (a,b, options) {
-        // console.log('event', a);
-        if (a.statusOption === b) {
-          return options.fn(this);
-        }
-      });
-      handlebars.registerHelper('ifEventMid', function (a,b, options) {
-        // console.log('event', a);
-        if (a.statusOption === b) {
-          return options.fn(this);
-        }
-      });
-
-      handlebars.registerHelper('ifEventGood', function (a,b, options) {
-        // console.log('event', a);
-        if (a.statusOption === b) {
-          return options.fn(this);
-        }
-      });
-
-      handlebars.registerHelper('ifGreen', function (a,b, options) {
-        const start = new Date();
-        const end = new Date(new Date(start).setMonth(start.getMonth() - 12));
-        console.log('startDate: ', start);
-        console.log('endDate: ', end);
-
-        console.log('eventDate', Date.parse(emailReplacements[0].eventDate) <= start);
-        let num = emailReplacements
-          .filter(events => Date.parse(events.eventDate) <= start || Date.parse(events.eventDate) >= end)
-          .filter(events => events.statusOption === 'Open' || events.statusOption === 'Pending');
-        console.log(num, num.length);
-
-        if (num.length < 2) {
-          console.log('GREEN');
-          return '<font color="#66BB6A"><b>GREEN</b></font>.<br><br>\n' +
-            'Thank you for your ongoing support.';
-        } else if (num.length >= 2 || num.length <= 4) {
-          console.log('YELLOW');
-          return '<font color="#FFEE58"><b>YELLOW</b></font>.<br><br>\n' +
-            'Please review all corrective actions on past issues and proactively look for common issues.\n';
-        } else if (num.length > 4) {
-          console.log('RED');
-          return '<font color="#EF5350"><b>RED</b></font>.<br><br>\n' +
-            'T&L QA will be contacting you to discuss an improvement plan.\n';
-        }
-      });
-
-      handlebars.registerHelper('formatTime', function (date, format) {
-        const mmnt = moment(date);
-        return mmnt.format(format);
-      });
+    setTimeout(
+      readHTMLFile(path.join(__dirname, '../models/html_templates/companyReport.html'), function (err, html) {
 
 
-      const template = handlebars.compile(html);
+        const emailReplacements = companyEvents;
 
-      const htmlToSend = template({eventItems: emailReplacements});
-
-      const mailContents = {
-        from: 'bill@tandlautomatics.com',
-        bcc: 'gwfreak01@gmail.com',
-        text: 'Email Reports from T&L Automatics',
-        html: htmlToSend
-      };
-
-
-      companyEmails[1].forEach(function (email, i, array) {
-        console.log('sending to: ', email);
-        console.log('emailReplacements: ', emailReplacements);
-        // console.log('emailList: ', emailList);
-        mailContents.to = email;
-        mailContents.subject = 'Quarterly Supplier Report';
-        // console.log(mailContents);
-
-        emailClient.sendMail(mailContents, function (err, info) {
-          if (err) {
-            console.log(err);
-            return;
-          } else {
-            console.log(i);
-            console.log('Message sent: %s %s', info.messageId, email);
-            // Preview only available when sending through an Ethereal account
-            // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-          }
-
-          if (i === emailList[1].length - 1) {
-            // mailContents.transport.close();
-            return res.status(200).json({
-              message: 'Emails sent successfully!',
-              emailList: emailList,
-              eventsList: eventsList
-            });
+        handlebars.registerHelper('ifEventBad', function (a,b, options) {
+          // console.log('event', a);
+          if (a.statusOption === b) {
+            return options.fn(this);
           }
         });
-      });
+        handlebars.registerHelper('ifEventMid', function (a,b, options) {
+          // console.log('event', a);
+          if (a.statusOption === b) {
+            return options.fn(this);
+          }
+        });
 
-    });
+        handlebars.registerHelper('ifEventGood', function (a,b, options) {
+          // console.log('event', a);
+          if (a.statusOption === b) {
+            return options.fn(this);
+          }
+        });
+
+        handlebars.registerHelper('ifGreen', function (a,b, options) {
+          const start = new Date();
+          const end = new Date(new Date(start).setMonth(start.getMonth() - 12));
+          console.log('startDate: ', start);
+          console.log('endDate: ', end);
+
+          console.log('eventDate', Date.parse(emailReplacements[0].eventDate) <= start);
+          let num = emailReplacements
+            .filter(events => Date.parse(events.eventDate) <= start || Date.parse(events.eventDate) >= end)
+            .filter(events => events.statusOption === 'Open' || events.statusOption === 'Pending');
+          console.log(num, num.length);
+
+          if (num.length < 2) {
+            console.log('GREEN');
+            return '<font color="#66BB6A"><b>GREEN</b></font>.<br><br>\n' +
+              'Thank you for your ongoing support.';
+          } else if (num.length >= 2 || num.length <= 4) {
+            console.log('YELLOW');
+            return '<font color="#FFEE58"><b>YELLOW</b></font>.<br><br>\n' +
+              'Please review all corrective actions on past issues and proactively look for common issues.\n';
+          } else if (num.length > 4) {
+            console.log('RED');
+            return '<font color="#EF5350"><b>RED</b></font>.<br><br>\n' +
+              'T&L QA will be contacting you to discuss an improvement plan.\n';
+          }
+        });
+
+        handlebars.registerHelper('formatTime', function (date, format) {
+          const mmnt = moment(date);
+          return mmnt.format(format);
+        });
+
+
+        const template = handlebars.compile(html);
+
+        const htmlToSend = template({eventItems: emailReplacements});
+
+        const mailContents = {
+          from: 'bill@tandlautomatics.com',
+          bcc: 'gwfreak01@gmail.com',
+          text: 'Email Reports from T&L Automatics',
+          html: htmlToSend
+        };
+
+
+        companyEmails[1].forEach(function (email, i, array) {
+          console.log('sending to: ', email);
+          console.log('emailReplacements: ', emailReplacements);
+          // console.log('emailList: ', emailList);
+          mailContents.to = email;
+          mailContents.subject = 'Quarterly Supplier Report';
+          // console.log(mailContents);
+
+          emailClient.sendMail(mailContents, function (err, info) {
+            if (err) {
+              console.log(err);
+              return;
+            } else {
+              console.log(i);
+              console.log('Message sent: %s %s', info.messageId, email);
+              // Preview only available when sending through an Ethereal account
+              // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+            }
+
+            if (i === emailList[1].length - 1) {
+              // mailContents.transport.close();
+              return res.status(200).json({
+                message: 'Emails sent successfully!',
+                emailList: emailList,
+                eventsList: eventsList
+              });
+            }
+          });
+        });
+
+      })
+
+      , 5000);
+
 
 
   });
