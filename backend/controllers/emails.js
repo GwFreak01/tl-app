52baa07be9818819914572add88aeea87825cd3e
GWFreak01 <gwfreak01@gmail.com>
2018-08-02 09:19:10 -0400

    Tests sendAllFeedback API call. Tests fullCompanyEmailsList forEach loop. Tests returned response after all emails are sent. Adds processedEmailReplacements to filter maximum of 1 year old events. Removes try catch block. Fixes console log after each successful sent email.
    * Fix QuarterlyCronJob to email all companies
    * Fix sendAllFeedback button function

--- backend/controllers/emails.js
+++ backend/controllers/emails.js
@@ -536,7 +536,7 @@ exports.sendAllFeedbackEmails = (req, res, next) => {
 
         } else {
           console.log(i);
-          console.log('Message sent: %s', emailGroup);
+          console.log('Message sent: %s', emailGroup[0]);
           // Preview only available when sending through an Ethereal account
           // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
