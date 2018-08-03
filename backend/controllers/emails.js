2346545501944dd9b158507ffff219115b08df6a
GWFreak01 <gwfreak01@gmail.com>
2018-08-02 23:16:14 -0400

    Tests sendAllFeedback API call. Adds Max to BCC for quarterly reports. Mass Email function sends out to emailGroups with validated past year only events. Tests quarterlyCronJob. Tests company and event queries on quarterlyCronJob converted to array via fetch method. Tests every  second.
    * Fix QuarterlyCronJob to email all companies
    * Fix sendAllFeedback button function

--- backend/controllers/emails.js
+++ backend/controllers/emails.js
@@ -10,6 +10,18 @@ const Event = require('../models/event');
 
 const CronJob = require('cron').CronJob;
 
+const readHTMLFile = function(path, callback) {
+  fs.readFile(path, {encoding: 'utf-8'}, function (err, html) {
+    if (err) {
+      throw err;
+      callback(err);
+    }
+    else {
+      callback(null, html);
+    }
+  });
+};
+
 try {
   new CronJob('00 00 6 1 1,4,7,10 *', function() {
     console.log('this should not be printed');
@@ -25,6 +37,8 @@ const emailQuarterlyJob = new CronJob({
   // seconds, minutes, hours, days of month, months, days of week
   // cronTime: '00 00 20 * * 0-6'
   // cronTime: '*/60 * * * * 0-6',
+
+  // Quarterly CronTime every Jan, April, Jul, Oct 1st at 5 AM on any day
   // cronTime: '0 0 5 1 1,4,7,10 *',
   // cronTime: '* * */24 * * 0-6',
   cronTime: '* * * * * *',
@@ -33,14 +47,13 @@ const emailQuarterlyJob = new CronJob({
 
 
     Company.find({},
-    function (err, companyDocuments) {
-      if (err) {
-        console.log('Could not query Company db');
-      }
-      // console.log('companyDocuments: ', companyDocuments);
-      companiesList = companyDocuments;
+      function (err, companyDocuments) {
+        if (err) {
+          console.log('Could not query Company db');
+        }
+        companiesList = companyDocuments;
       }).then(() => {
-      // console.log('companies: ', companiesList);
+        // console.log('companies: ', companiesList);
       }
     );
 
@@ -49,36 +62,180 @@ const emailQuarterlyJob = new CronJob({
         if (err) {
           console.log('Could not query Company db');
         }
-        // console.log('companyDocuments: ', companyDocuments);
         eventsList = eventDocuments;
       }).then(() => {
-      // console.log('events: ', eventsList);
+        // console.log('events: ', eventsList);
       }
     );
 
     console.log('companies: ', companiesList);
     console.log('events: ', eventsList);
 
-    // const eventsList = Event.find().fetch();
-    // const companiesList = Company.find({}, function (err, companyDocuments) {
-    //   if (err) {
-    //     console.log('Could not query Company db');
-    //   }
-    //   return companyDocuments;
-    // });
-    // const eventsList = Event.find().toArray(function (err, eventDocuments) {
-    //   if (err) {
-    //     console.log('Could not query Event db');
-    //   }
-    //   return eventDocuments;
-    // });
+    let fullCompanyEmailsList = [];
+    let fullCompanyEventsList = [];
 
-    // const test = Company.find().sub
-    // console.log('companies: ', companiesList );
-    // console.log('events: ', eventsList);
+    // console.log('AllFeedbackCompanies: ', companies);
+    // console.log('AllFeedbackEvents: ', events);
+
+
+    // const companies = companiesList;
+    // let
+      companiesList.forEach((company, index, array) => {
+      let partialCompanyEmailsList = [];
+
+      // console.log('forEach: ', company);
+
+      let partialCompanyEventsList = events.filter(event => event.companyName == company.companyName);
+
+      if (company.salesPerson.status) {
+        partialCompanyEmailsList.push(company.salesPerson.email);
+      }
+      if (company.qualityPerson.status) {
+        partialCompanyEmailsList.push(company.qualityPerson.email);
+      }
+      if (company.logisticsPerson.status) {
+        partialCompanyEmailsList.push(company.logisticsPerson.email);
+      }
+      if (company.differentPerson.status) {
+        partialCompanyEmailsList.push(company.differentPerson.email);
+      }
+      fullCompanyEmailsList.push(partialCompanyEmailsList);
+      // console.log('companyEmails: ', companyEmails);
+
+      fullCompanyEventsList.push(partialCompanyEventsList);
+
+      // setTimeout(() =>{}
+      //   , 2500);
+
+
+
+    });
+
+
+    console.log('fullCompanyEmailsList: ', fullCompanyEmailsList);
+    console.log('fullCompanyEventsList: ', fullCompanyEventsList);
+
+    fullCompanyEmailsList.forEach((emailGroup, index, array) => {
+      if (emailGroup.length == 0) {
+        return;
+      }
+      console.log('index: ', index);
+
+      readHTMLFile(path.join(__dirname, '../models/html_templates/companyReport.html'), function (err, html) {
+
+
+        const emailReplacements = fullCompanyEventsList[index];
+        const start = new Date();
+        const end = new Date(new Date(start).setMonth(start.getMonth() - 12));
+
+        let processedEmailReplacements = emailReplacements
+          .filter(events => Date.parse(events.eventDate) <= start && Date.parse(events.eventDate) >= end)
+          .filter(events => events.statusOption === 'Open' || events.statusOption === 'Pending');
+
+        // console.log('emailReplacements: ', emailReplacements);
+        handlebars.registerHelper('ifEventBad', function (a, b, options) {
+          // console.log('event', a);
+          if (a.statusOption === b) {
+            return options.fn(this);
+          }
+        });
+        handlebars.registerHelper('ifEventMid', function (a, b, options) {
+          // console.log('event', a);
+          if (a.statusOption === b) {
+            return options.fn(this);
+          }
+        });
+
+        handlebars.registerHelper('ifEventGood', function (a, b, options) {
+          // console.log('event', a);
+          if (a.statusOption === b) {
+            return options.fn(this);
+          }
+        });
+
+        handlebars.registerHelper('ifGreen', function (a, b, options) {
+          console.log('startDate: ', start);
+          console.log('endDate: ', end);
+
+          // try {
+          //   console.log('eventDate', Date.parse(emailReplacements[0].eventDate) <= start);
+          //
+          // } catch (e) {
+          //   return;
+          // }
+          // let num = emailReplacements
+          //   .filter(events => Date.parse(events.eventDate) <= start && Date.parse(events.eventDate) >= end)
+          //   .filter(events => events.statusOption === 'Open' || events.statusOption === 'Pending');
+          console.log('num: %s \n numLength: %s', processedEmailReplacements, processedEmailReplacements.length);
+
+          if (processedEmailReplacements.length < 2) {
+            console.log('GREEN');
+            return '<font color="#66BB6A"><b>GREEN</b></font>.<br><br>\n' +
+              'Thank you for your ongoing support.';
+          } else if (processedEmailReplacements.length >= 2 || processedEmailReplacements.length <= 4) {
+            console.log('YELLOW');
+            return '<font color="#FFEE58"><b>YELLOW</b></font>.<br><br>\n' +
+              'Please review all corrective actions on past issues and proactively look for common issues.\n';
+          } else if (processedEmailReplacements.length > 4) {
+            console.log('RED');
+            return '<font color="#EF5350"><b>RED</b></font>.<br><br>\n' +
+              'T&L QA will be contacting you to discuss an improvement plan.\n';
+          }
+        });
+
+        handlebars.registerHelper('formatTime', function (date, format) {
+          const mmnt = moment(date);
+          return mmnt.format(format);
+        });
+
+
+        const template = handlebars.compile(html);
+
+        const htmlToSend = template({eventItems: processedEmailReplacements});
+
+        const mailContents = {
+          from: 'bill@tandlautomatics.com',
+          bcc: ['gwfreak01@gmail.com', 'max@tandlautomatics.com'],
+          text: 'Email Reports from T&L Automatics',
+          html: htmlToSend
+        };
+
+
+        // emailList[1].forEach(function (email, i, array) {
+        console.log('sending to: ', emailGroup);
+        console.log('individualCompanyEvents:', processedEmailReplacements);
+
+        // console.log('emailReplacements: ', emailReplacements);
+        // console.log('emailList: ', emailList);
+        mailContents.to = emailGroup;
+        mailContents.subject = 'Quarterly Supplier Report';
+        // console.log(mailContents);
+
+        emailClient.sendMail(mailContents, function (err, info) {
+          if (err) {
+            console.log(err);
+
+          } else {
+            // console.log(i);
+            console.log('Message sent: %s %s', info.messageId, emailGroup);
+            // Preview only available when sending through an Ethereal account
+            // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+          }
+        });
+
+
+        // });
+
+      });
+      if (index === fullCompanyEmailsList.length - 1) {
+        // mailContents.transport.close();
+        console.log('All Quarterly Emails sent');
+      }
+    });
+
 
   },
-  start: false,
+  start: true,
   timezone: 'America/New_York'
 
 });
@@ -94,17 +251,7 @@ const mailgunOptions = {
   }
 };
 
-const readHTMLFile = function(path, callback) {
-  fs.readFile(path, {encoding: 'utf-8'}, function (err, html) {
-    if (err) {
-      throw err;
-      callback(err);
-    }
-    else {
-      callback(null, html);
-    }
-  });
-};
+
 
 // MailGun Config
 
@@ -449,14 +596,8 @@ exports.sendAllFeedbackEmails = (req, res, next) => {
           console.log('Message sent: %s %s', info.messageId, emailGroup);
           // Preview only available when sending through an Ethereal account
           // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
         }
-
-
       });
-
-
-      // });
 
     });
     if (index === fullCompanyEmailsList.length - 1) {
@@ -468,128 +609,5 @@ exports.sendAllFeedbackEmails = (req, res, next) => {
       });
     }
   });
-
-  // eventsList.forEach((event, index, array) => {
-  //   console.log('forEachEvent: ', event);
-  //   // console.log('forEachArray: ', array);
-  //   if (event[1].length == 0) {
-  //     console.log('%s does not have events', event[0]);
-  //   }
-  //   else if (event[1].length != 0 && emailList[1].length != 0) {
-  //     console.log('%s has events: ', event[0]);
-  //     console.log('companyEvents: ', event[1]);
-  //
-  // readHTMLFile(path.join(__dirname, '../models/html_templates/companyReport.html'), function (err, html) {
-  //
-  //
-  //   const emailReplacements = event[1];
-  //   console.log('ReadFilecompanyEvents:', emailReplacements);
-  //   handlebars.registerHelper('ifEventBad', function (a, b, options) {
-  //     // console.log('event', a);
-  //     if (a.statusOption === b) {
-  //       return options.fn(this);
-  //     }
-  //   });
-  //   handlebars.registerHelper('ifEventMid', function (a, b, options) {
-  //     // console.log('event', a);
-  //     if (a.statusOption === b) {
-  //       return options.fn(this);
-  //     }
-  //   });
-  //
-  //   handlebars.registerHelper('ifEventGood', function (a, b, options) {
-  //     // console.log('event', a);
-  //     if (a.statusOption === b) {
-  //       return options.fn(this);
-  //     }
-  //   });
-  //
-  //   handlebars.registerHelper('ifGreen', function (a, b, options) {
-  //     const start = new Date();
-  //     const end = new Date(new Date(start).setMonth(start.getMonth() - 12));
-  //     console.log('startDate: ', start);
-  //     console.log('endDate: ', end);
-  //
-  //     console.log('eventDate', Date.parse(emailReplacements[0].eventDate) <= start);
-  //     let num = emailReplacements
-  //       .filter(events => Date.parse(events.eventDate) <= start || Date.parse(events.eventDate) >= end)
-  //       .filter(events => events.statusOption === 'Open' || events.statusOption === 'Pending');
-  //     console.log(num, num.length);
-  //
-  //     if (num.length < 2) {
-  //       console.log('GREEN');
-  //       return '<font color="#66BB6A"><b>GREEN</b></font>.<br><br>\n' +
-  //         'Thank you for your ongoing support.';
-  //     } else if (num.length >= 2 || num.length <= 4) {
-  //       console.log('YELLOW');
-  //       return '<font color="#FFEE58"><b>YELLOW</b></font>.<br><br>\n' +
-  //         'Please review all corrective actions on past issues and proactively look for common issues.\n';
-  //     } else if (num.length > 4) {
-  //       console.log('RED');
-  //       return '<font color="#EF5350"><b>RED</b></font>.<br><br>\n' +
-  //         'T&L QA will be contacting you to discuss an improvement plan.\n';
-  //     }
-  //   });
-  //
-  //   handlebars.registerHelper('formatTime', function (date, format) {
-  //     const mmnt = moment(date);
-  //     return mmnt.format(format);
-  //   });
-  //
-  //
-  //   const template = handlebars.compile(html);
-  //
-  //   const htmlToSend = template({eventItems: emailReplacements});
-  //
-  //   const mailContents = {
-  //     from: 'bill@tandlautomatics.com',
-  //     // bcc: 'gwfreak01@gmail.com',
-  //     text: 'Email Reports from T&L Automatics',
-  //     html: htmlToSend
-  //   };
-  //
-  //
-  //   emailList[1].forEach(function (email, i, array) {
-  //     console.log('sending to: ', email);
-  //     console.log('emailReplacements: ', emailReplacements);
-  //     // console.log('emailList: ', emailList);
-  //     mailContents.to = email;
-  //     mailContents.subject = 'Quarterly Supplier Report';
-  //     // console.log(mailContents);
-  //
-  //     emailClient.sendMail(mailContents, function (err, info) {
-  //       if (err) {
-  //         console.log(err);
-  //         return;
-  //       } else {
-  //         console.log(i);
-  //         console.log('Message sent: %s %s', info.messageId, email);
-  //         // Preview only available when sending through an Ethereal account
-  //         // console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-  //
-  //       }
-  //
-  //       if (i === emailList[1].length - 1) {
-  //         // mailContents.transport.close();
-  //         return res.status(200).json({
-  //           message: 'Emails sent successfully!',
-  //           emailList: emailList,
-  //           eventsList: eventsList
-  //         });
-  //       }
-  //     });
-  //   });
-  //
-  // });
-  //
-  //   }
-  // });
-
-
-  // return res.status(200).json({
-  //   message: 'Emails sent successfully!',
-  //   emailList: fullCompanyEmailsList,
-  //   eventsList: fullCompanyEventsList
-  // });
 
 };
