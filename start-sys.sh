33aee9f223e8b84499424bab1ddae4c7347af401
GWFreak01 <gwfreak01@gmail.com>
2018-09-06 11:39:55 -0500

    Adds BackupDB bash file to run backup service

--- start-sys.sh
+++ start-sys.sh
@@ -13,6 +13,7 @@ killall python
 
 mate-terminal --geometry=100x24+20+20 --zoom=1 --hide-menubar -e 'sh /home/tl/Documents/tl-app/Backend.sh' &
 mate-terminal --geometry=100x24+20+400 --zoom=1 --hide-menubar -e 'sh /home/tl/Documents/tl-app/Frontend.sh' &
+mate-terminal --geometry=100x24+20+400 --zoom=1 --hide-menubar -e 'sh /home/tl/Documents/tl-app/BackupDB.sh' &
 
 
 
