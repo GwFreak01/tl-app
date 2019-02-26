d22b046bf5aca021117598cd10451514841e64f5
T-and-L <44841062+T-and-L@users.noreply.github.com>
2019-02-26 11:51:05 -0500

    Fix Errno32 python->php

--- Frontend.sh
+++ Frontend.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
 cd /home/tl/Documents/tl-app/dist/tl-app
-python -m SimpleHTTPServer 4200
+php -S localhost:4200
+
+# Switched to php self-serve instead of python...
+# python -m SimpleHTTPServer 4200
+
+# to avoid Errno32 -- Max @ 2/26/2019
+
